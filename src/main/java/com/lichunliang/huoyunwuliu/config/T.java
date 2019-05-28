4e75eaa7978b7557e16495ebf9b486c308244d02
lcl <269011781@qq.com>
2019-05-28 19:24:26 +0800

    //1

---
+++ src/main/java/com/lichunliang/huoyunwuliu/config/T.java
@@ -0,0 +1,40 @@
+package com.lichunliang.huoyunwuliu.config;
+
+/**
+ * ━━━━━━━━━神兽出没━━━━━━━━━
+ * <p>
+ * 　　　　　　　　┏┓　　　┏┓+ +
+ * 　　　　　　　┏┛┻━━━┛┻┓ + +
+ * 　　　　　　　┃　　　　　　　┃
+ * 　　　　　　　┃　　　━　　　┃ ++ + + +
+ * 　　　　　　 ████━████ ┃+
+ * 　　　　　　　┃　　　　　　　┃ +
+ * 　　　　　　　┃　　　┻　　　┃
+ * 　　　　　　　┃　　　　　　　┃ + +
+ * 　　　　　　　┗━┓　　　┏━┛
+ * 　　　　　　　　　┃　　　┃
+ * 　　　　　　　　　┃　　　┃ + + + +
+ * 　　　　　　　　　┃　　　┃　　　　Code is far away from bug with the animal protecting
+ * 　　　　　　　　　┃　　　┃ + 　　　　神兽保佑,代码无bug
+ * 　　　　　　　　　┃　　　┃
+ * 　　　　　　　　　┃　　　┃　　+
+ * 　　　　　　　　　┃　 　　┗━━━┓ + +
+ * 　　　　　　　　　┃ 　　　　　　　┣┓
+ * 　　　　　　　　　┃ 　　　　　　　┏┛
+ * 　　　　　　　　　┗┓┓┏━┳┓┏┛ + + + +
+ * 　　　　　　　　　　┃┫┫　┃┫┫
+ * 　　　　　　　　　　┗┻┛　┗┻┛+ + + +
+ * <p>
+ * ━━━━━━━━━感觉萌萌哒━━━━━━━━━
+ * 类说明：
+ */
+public class T {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("***");
+
+    }
+
+}
